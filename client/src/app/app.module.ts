539df5af9358328c67b8a61d1c4f837ddb9dbf2c
Elana Olson <elanaolson@gmail.com>
2019-11-03 21:43:38 -0800

    connect frontend to backend with http client

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+<<<<<<< 873c1d2ae9014630b7860ff02a47d890fe7c717a
 
 import { MatMenuModule } from '@angular/material/menu';
 import { MatCardModule } from '@angular/material/card';
@@ -8,6 +9,15 @@ import { MatCardModule } from '@angular/material/card';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LandingComponent } from './landing/landing.component';
+=======
+import { MatMenuModule } from '@angular/material/menu';
+import { MatCardModule } from '@angular/material/card';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { LandingComponent } from './landing/landing.component';
+import { ConnectionService } from './connection.service';
+>>>>>>> add http client
 
 @NgModule({
   declarations: [
@@ -21,7 +31,7 @@ import { LandingComponent } from './landing/landing.component';
     MatMenuModule,
     MatCardModule
   ],
-  providers: [],
+  providers: [ConnectionService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
---
+++ client/src/app/connection.service.ts
@@ -0,0 +1,12 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ConnectionService {
+
+  uri = 'http://localhost:8080';
+
+  constructor(private http: HttpClient) { }
+}
