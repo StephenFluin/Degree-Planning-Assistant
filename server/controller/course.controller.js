5d21fe3aeb18ca66d91f8a0c445424dd772f60aa
Elana Olson <elanaolson@gmail.com>
2020-02-17 15:07:43 -0800

    fix corequisite spelling

--- server/controller/course.controller.js
+++ server/controller/course.controller.js
@@ -1,15 +1,8 @@
 import express from 'express';
-import {
-  check,
-  validationResult
-} from 'express-validator';
-import passport from "passport";
-import {
-  generateServerErrorCode
-} from '../store/utils';
-import {
-  Course
-} from '../database/models';
+import { check, validationResult } from 'express-validator';
+import passport from 'passport';
+import { generateServerErrorCode } from '../store/utils';
+import { Course } from '../database/models';
 import {
   COURSE_SCHOOL_IS_EMPTY,
   COURSE_EXISTS_ALREADY,
@@ -19,7 +12,7 @@ import {
   COURSE_TITLE_IS_EMPTY,
   COURSE_DESCRIPTION_IS_EMPTY,
   COURSE_TERMS_OFFERED_IS_EMPTY,
-  SOME_THING_WENT_WRONG
+  SOME_THING_WENT_WRONG,
 } from './constant';
 
 const courseController = express.Router();
@@ -27,60 +20,67 @@ const ObjectId = require('mongoose').Types.ObjectId;
 
 const validateCourseCreation = [
   check('school')
-  .exists()
-  .withMessage(COURSE_SCHOOL_IS_EMPTY),
+    .exists()
+    .withMessage(COURSE_SCHOOL_IS_EMPTY),
   check('code')
-  .exists()
-  .withMessage(COURSE_CODE_IS_EMPTY),
+    .exists()
+    .withMessage(COURSE_CODE_IS_EMPTY),
   check('title')
-  .exists()
-  .withMessage(COURSE_TITLE_IS_EMPTY),
+    .exists()
+    .withMessage(COURSE_TITLE_IS_EMPTY),
   check('description')
-  .exists()
-  .withMessage(COURSE_DESCRIPTION_IS_EMPTY),
+    .exists()
+    .withMessage(COURSE_DESCRIPTION_IS_EMPTY),
   check('termsOffered')
-  .exists()
-  .withMessage(COURSE_TERMS_OFFERED_IS_EMPTY)
+    .exists()
+    .withMessage(COURSE_TERMS_OFFERED_IS_EMPTY),
 ];
 
 const validateSingleCourse = [
   check('school')
-  .exists()
-  .withMessage(COURSE_SCHOOL_IS_EMPTY),
+    .exists()
+    .withMessage(COURSE_SCHOOL_IS_EMPTY),
   check('code')
-  .exists()
-  .withMessage(COURSE_CODE_IS_EMPTY)
-  .matches(/\d/)
-  .withMessage(DOES_NOT_CONTAIN_A_DIGIT)
+    .exists()
+    .withMessage(COURSE_CODE_IS_EMPTY)
+    .matches(/\d/)
+    .withMessage(DOES_NOT_CONTAIN_A_DIGIT),
 ];
 
 const validateSchoolCourses = [
   check('school')
-  .exists()
-  .withMessage(COURSE_SCHOOL_IS_EMPTY),
+    .exists()
+    .withMessage(COURSE_SCHOOL_IS_EMPTY),
 ];
 
 /**
  * Creates a new Course with correct reference to ObjectId of prerequisites and corequisites
- * @param {String} schl 
- * @param {String} code 
- * @param {String} title 
- * @param {String} description 
- * @param {[ObjectId]} prereq 
- * @param {[ObjectId]} coreq 
- * @param {Number} difficulty 
- * @param {Number} impaction 
- * @param {String} termsOffered 
+ * @param {String} schl
+ * @param {String} code
+ * @param {String} title
+ * @param {String} description
+ * @param {[ObjectId]} prereq
+ * @param {[ObjectId]} coreq
+ * @param {Number} difficulty
+ * @param {Number} impaction
+ * @param {String} termsOffered
  * @returns {Course} new course
  */
-async function createCourse(schl, code, title, description, prereq, coreq, difficulty, impaction, termsOffered) {
-  console.log("Current course: ", schl, code);
-  let course;
-  let prerequisites;
+async function createCourse(
+  schl,
+  code,
+  title,
+  description,
+  prereq,
+  coreq,
+  difficulty,
+  impaction,
+  termsOffered
+) {
   try {
-    prerequisites = await loopThroughCourses(schl, prereq);
-    let corequisites = coreq;
-    // let corequisites = await loopThroughCourses(schl, coreq);
+    let course;
+    let prerequisites = await loopThroughCourses(schl, prereq);
+    let corequisites = await loopThroughCourses(schl, coreq);
     const data = {
       school: schl,
       code,
@@ -90,49 +90,47 @@ async function createCourse(schl, code, title, description, prereq, coreq, diffi
       corequisites,
       difficulty,
       impaction,
-      termsOffered
+      termsOffered,
     };
-
     course = new Course(data);
     return course.save();
-
   } catch (e) {
-    console.log("looping error", e);
+    console.log('createCourse() Error: ', e);
   }
 }
 
 /**
  * Loop though an array of Courses to get the ObjectId of each
- * @param {String} school 
- * @param {[String]} codes 
+ * @param {String} school
+ * @param {[String]} codes
  * @returns {[Course]} coursesWithIds
  */
-async function loopThroughCourses(school, codes) {
+async function loopThroughCourses(school, codes, next) {
   try {
     let coursesWithIds = [];
     for (let code of codes) {
-      const objId = await getObjectId(school, code)
+      const objId = await getObjectId(school, code);
       coursesWithIds.push(objId);
     }
     return coursesWithIds;
   } catch (e) {
-    console.log("The error e: ", e);
+    console.log('loopThroughCourses() Error: ', e);
   }
 }
 
 /**
  * Get the ObjectId of a course based on school (schl) and course code
  * Or initialize a new course and retrieve the ObjectId
- * @param {String} schl 
- * @param {String} courseCode 
+ * @param {String} schl
+ * @param {String} courseCode
  * @returns {ObjectId} course._id
  */
 async function getObjectId(school, code) {
-  console.log("getObjectId of school and code: ", school, code);
+  console.log('getObjectId of school and code: ', school, code);
   try {
     let course = await Course.findOne({
       school,
-      code
+      code,
     });
     if (!course) {
       const createdCourse = await createEmptyCourse(school, code);
@@ -140,32 +138,31 @@ async function getObjectId(school, code) {
     }
     return new ObjectId(course._id);
   } catch (e) {
-    console.log("The unhandled error: ", e);
+    console.log('getObjectId() Error: ', e);
   }
 }
 
 /**
  * Initialiazes an empty course for future use
- * @param {String} schl 
- * @param {String} courseCode 
+ * @param {String} schl
+ * @param {String} courseCode
  * @returns {Course} new course
  */
-async function createEmptyCourse(schl, courseCode) {
+async function createEmptyCourse(school, code) {
   const data = {
-    school: schl,
-    code: courseCode,
-    title: " ",
-    description: " ",
+    school,
+    code,
+    title: ' ',
+    description: ' ',
     prerequisites: [],
     corequisites: [],
     difficulty: 0,
     impaction: 0,
-    termsOffered: " "
+    termsOffered: ' ',
   };
   let newCourse = new Course(data);
   return newCourse.save();
 }
-
 
 // function populateCourseBasedOnId() {
 //   .populate('prerequisites')
@@ -179,16 +176,18 @@ async function createEmptyCourse(schl, courseCode) {
  * POST/
  * Create a new course, requires authentication of user
  */
-courseController.post('/create',
+courseController.post(
+  '/create',
   passport.authenticate('jwt', {
-    session: false
+    session: false,
   }),
-  validateCourseCreation, async (req, res) => {
+  validateCourseCreation,
+  async (req, res) => {
     const errorsAfterValidation = validationResult(req);
     if (!errorsAfterValidation.isEmpty()) {
       res.status(400).json({
         code: 400,
-        errors: errorsAfterValidation.mapped()
+        errors: errorsAfterValidation.mapped(),
       });
     } else {
       try {
@@ -201,58 +200,118 @@ courseController.post('/create',
           corequisites,
           difficulty,
           impaction,
-          termsOffered
+          termsOffered,
         } = req.body;
 
         // Check if course already exists
         const course = await Course.findOne({
           school,
-          code
+          code,
         });
 
         // It doesn't, so create a new course
         if (!course) {
-          const createdCourse = await createCourse(school, code, title, description, prerequisites, corequisites, difficulty, impaction, termsOffered);
+          const createdCourse = await createCourse(
+            school,
+            code,
+            title,
+            description,
+            prerequisites,
+            corequisites,
+            difficulty,
+            impaction,
+            termsOffered
+          );
           const courseToReturn = {
-            ...createdCourse.toJSON()
+            ...createdCourse.toJSON(),
           };
           res.status(200).json(courseToReturn);
         } else {
-          generateServerErrorCode(res, 403, 'course creation error', COURSE_EXISTS_ALREADY, 'code');
+          generateServerErrorCode(
+            res,
+            403,
+            'course creation error',
+            COURSE_EXISTS_ALREADY,
+            'code'
+          );
         }
       } catch (e) {
         generateServerErrorCode(res, 500, e, SOME_THING_WENT_WRONG);
       }
     }
-  });
+  }
+);
 
 /**
  * GET/
  * Get a single course based on school and code
  */
-courseController.get('/:school/:code', validateSingleCourse, async (req, res) => {
+courseController.get(
+  '/:school/:code',
+  validateSingleCourse,
+  async (req, res) => {
+    const errorsAfterValidation = validationResult(req);
+    if (!errorsAfterValidation.isEmpty()) {
+      res.status(400).json({
+        code: 400,
+        errors: errorsAfterValidation.mapped(),
+      });
+    } else {
+      try {
+        const { school, code } = req.params;
+        const course = await Course.findOne({
+          school,
+          code,
+        });
+
+        if (course) {
+          const courseToReturn = course;
+          res.status(200).json(courseToReturn);
+        } else {
+          generateServerErrorCode(
+            res,
+            403,
+            'course retrieval error',
+            COURSE_DOES_NOT_EXIST,
+            'code'
+          );
+        }
+      } catch (e) {
+        generateServerErrorCode(res, 500, e, SOME_THING_WENT_WRONG);
+      }
+    }
+  }
+);
+
+/**
+ * GET/
+ * Get all courses for a school
+ */
+courseController.get('/:school', validateSchoolCourses, async (req, res) => {
   const errorsAfterValidation = validationResult(req);
   if (!errorsAfterValidation.isEmpty()) {
     res.status(400).json({
       code: 400,
-      errors: errorsAfterValidation.mapped()
+      errors: errorsAfterValidation.mapped(),
     });
   } else {
     try {
-      const {
-        school,
-        code
-      } = req.params;
-      const course = await Course.findOne({
-        school,
-        code
+      const courses = await Course.find({
+        school: req.params.school,
       });
-
-      if (course) {
-        const courseToReturn = course;
-        res.status(200).json(courseToReturn);
+      if (courses) {
+        console.log('\n---COURSES---\n ', courses);
+        const coursesToReturn = courses;
+        console.log('\n---COURSES TO RETURN---\n ', coursesToReturn);
+        res.status(200).json(coursesToReturn);
       } else {
-        generateServerErrorCode(res, 403, 'course retrieval error', COURSE_DOES_NOT_EXIST, 'code');
+        generateServerErrorCode(
+          res,
+          403,
+          'course retrieval error',
+          COURSE_DOES_NOT_EXIST,
+          'code'
+        );
       }
     } catch (e) {
       generateServerErrorCode(res, 500, e, SOME_THING_WENT_WRONG);
@@ -264,30 +323,13 @@ courseController.get('/:school/:code', validateSingleCourse, async (req, res) =>
  * GET/
  * Get all courses for a school
  */
-courseController.get('/:school', validateSchoolCourses, async (req, res) => {
-  const errorsAfterValidation = validationResult(req);
-  if (!errorsAfterValidation.isEmpty()) {
-    res.status(400).json({
-      code: 400,
-      errors: errorsAfterValidation.mapped()
-    });
-  } else {
-    try {
-      const courses = await Course.find({
-        school: req.params.school
-      });
-      if (courses) {
-        console.log("\n---COURSES---\n ", courses);
-        const coursesToReturn = courses;
-        console.log("\n---COURSES TO RETURN---\n ", coursesToReturn);
-        res.status(200).json(coursesToReturn);
-      } else {
-        generateServerErrorCode(res, 403, 'course retrieval error', COURSE_DOES_NOT_EXIST, 'code');
-      }
-    } catch (e) {
-      generateServerErrorCode(res, 500, e, SOME_THING_WENT_WRONG);
+courseController.delete('/', async (req, res) => {
+  Course.deleteMany({}, function(err, result) {
+    if (err) {
+      return res.status(500).json({ err });
     }
-  }
+    return res.status(200).json({ message: 'delete all course' });
+  });
 });
 
 export default courseController;
--- server/database/schemas/course.schema.js
+++ server/database/schemas/course.schema.js
@@ -1,15 +1,19 @@
 import { Schema } from 'mongoose';
 
 const courseSchema = new Schema({
-  school: {type: String, required: true, uppercase: true },
+  school: { type: String, required: true, uppercase: true },
   code: { type: String, required: true, uppercase: true },
   title: { type: String, required: true },
   description: { type: String, required: true },
-  prerequisites: [ { type: Schema.Types.ObjectId, ref: 'Course' , uppercase: true } ],
-  corequisities: [ { type: Schema.Types.ObjectId, ref: 'Course' , uppercase: true } ],
+  prerequisites: [
+    { type: Schema.Types.ObjectId, ref: 'Course', uppercase: true },
+  ],
+  corequisites: [
+    { type: Schema.Types.ObjectId, ref: 'Course', uppercase: true },
+  ],
   difficulty: { type: Number },
   impaction: { type: Number },
-  termsOffered: { type: String, required: true }
+  termsOffered: { type: String, required: true },
 });
 
 export default courseSchema;
