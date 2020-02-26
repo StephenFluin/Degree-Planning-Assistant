cba3553ad99e5c9986458e719d2e0a66dc62a5fe
Elana Olson <elanaolson@gmail.com>
2020-02-26 15:50:07 -0800

    get courses taken and display

--- client/src/app/s-courses-taken/s-courses-taken.component.ts
+++ client/src/app/s-courses-taken/s-courses-taken.component.ts
@@ -1,6 +1,5 @@
 import { Component, OnInit } from "@angular/core";
 import { UserService, UserProfile } from "../user.service";
-import { CourseService } from "../course.service";
 import { ErrorHandlerService } from "../error-handler.service";
 import { Observable } from "rxjs";
 
@@ -10,40 +9,10 @@ import { Observable } from "rxjs";
   styleUrls: ["./s-courses-taken.component.css"]
 })
 export class SCoursesTakenComponent implements OnInit {
-  coursesTaken;
-  constructor(
-    private userService: UserService,
-    private courseService: CourseService,
-    private errorHandler: ErrorHandlerService
-  ) {
-    this.coursesTaken = courseService.getAllCourses();
+  profile: Observable<UserProfile>;
+  constructor(private userService: UserService) {
+    this.profile = this.userService.getProfile();
   }
 
   ngOnInit() {}
-
-  getCurrentProfile() {
-    const profileObs: Observable<UserProfile> = this.userService.getProfile();
-
-    // retrieve token or handle server error
-    profileObs.subscribe(
-      result => {
-        // HTTP result: config and data:
-        console.log("Profile result: ", result);
-        console.log(result.courses_taken);
-        return result;
-      },
-      err => {
-        // HTTP error
-        this.errorHandler.handleError(err);
-      },
-      () => {
-        // HTTP request completed
-        console.log("Profile data: ", profileObs);
-      }
-    );
-
-    console.log("obser", profileObs);
-  }
-
-  addCourse() {}
 }
