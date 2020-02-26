6011ea511bca324eea1d2e99cfd84a5ad60f51af
Elana Olson <elanaolson@gmail.com>
2020-02-26 13:48:14 -0800

    get current profile

--- client/src/app/s-courses-taken/s-courses-taken.component.ts
+++ client/src/app/s-courses-taken/s-courses-taken.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from "@angular/core";
 import { UserService } from "../user.service";
 import { CourseService } from "../course.service";
+import { ErrorHandlerService } from "../error-handler.service";
 
 @Component({
   selector: "app-s-courses-taken",
@@ -11,10 +12,36 @@ export class SCoursesTakenComponent implements OnInit {
   coursesTaken;
   constructor(
     private userService: UserService,
-    private courseService: CourseService
+    private courseService: CourseService,
+    private errorHandler: ErrorHandlerService
   ) {
     this.coursesTaken = courseService.getAllCourses();
   }
 
   ngOnInit() {}
+
+  getCurrentProfile() {
+    const profileObs = this.userService.getProfile();
+
+    // retrieve token or handle server error
+    profileObs.subscribe(
+      result => {
+        // HTTP result: config and data:
+        console.log("Profile result: ", result);
+        return result;
+      },
+      err => {
+        // HTTP error
+        this.errorHandler.handleError(err);
+      },
+      () => {
+        // HTTP request completed
+        console.log("Profile data: ", profileObs);
+      }
+    );
+
+    console.log("obser", profileObs);
+  }
+
+  addCourse() {}
 }
