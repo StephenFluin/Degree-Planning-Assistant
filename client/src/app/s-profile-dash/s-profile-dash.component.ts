9e0e6282ef0da015f1765375fcf17ec4271ebbfa
DaleCS <Dale.Seen@gmail.com>
2020-03-01 21:51:48 -0800

    020: Display Profile on Dashboard and Profile Page

---
+++ client/src/app/s-profile-dash/s-profile-dash.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-s-profile-dash',
+  templateUrl: './s-profile-dash.component.html',
+  styleUrls: ['./s-profile-dash.component.css']
+})
+export class SProfileDashComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
---
+++ client/src/app/s-profile/s-profile.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-s-profile',
+  templateUrl: './s-profile.component.html',
+  styleUrls: ['./s-profile.component.css']
+})
+export class SProfileComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
