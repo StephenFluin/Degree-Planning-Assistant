66cc5821f6c3bda87e8bc14638ab1f7dd1bf51d8
DaleCS <Dale.Seen@gmail.com>
2020-03-06 08:53:56 -0800

    Added property binding for profile fields

--- client/src/app/s-profile/s-profile.component.ts
+++ client/src/app/s-profile/s-profile.component.ts
@@ -1,15 +1,31 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { UserService, UserProfile } from "../user.service";
+import { Observable } from "rxjs";
 
 @Component({
-  selector: 'app-s-profile',
-  templateUrl: './s-profile.component.html',
-  styleUrls: ['./s-profile.component.css']
+  selector: "app-s-profile",
+  templateUrl: "./s-profile.component.html",
+  styleUrls: ["./s-profile.component.css"]
 })
 export class SProfileComponent implements OnInit {
+  // profile: Observable<UserProfile>;
+  profile = {
+    firstName: "Dale Christian",
+    lastName: "Seen",
+    major: "Software Engineering",
+    minor: "---",
+    school: "San Jose State University",
+    email: "Dale.Seen@gmail.com",
+    catalogYear: 2017,
+    expectedGradTerm: "Spring",
+    expectedGradYear: 2020,
+    bio: "Kimi no sei, Kimi no sei, Kimi no sei!"
+  };
 
-  constructor() { }
-
-  ngOnInit() {
+  constructor(private userService: UserService) {
+    // TODO: Set profile to refer to profile in userService
+    // this.profile = this.userService.getProfile();
   }
 
+  ngOnInit() {}
 }
